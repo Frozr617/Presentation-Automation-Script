a51c7f41de2ae3e0b750ddb399e2a8a0b4233120
Frozr617 <umax2006@outlook.com>
2019-10-24 19:25:42 +0300

    Bug with pasting background image

--- Main.java
+++ Main.java
@@ -9,6 +9,8 @@ import java.nio.file.Path;
 import java.nio.file.Files;
 import java.lang.IllegalArgumentException;
 import java.lang.NullPointerException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 
 public class Main{
 
@@ -36,9 +38,38 @@ public class Main{
     }
   }
   public static void takingDataForSlide() {
-    System.out.println("Type the text for your slide: ");
-    Scanner slideTextTaker = new Scanner(System.in);
-    String slideTest = slideTextTaker.nextLine();
-    File textFile = new File("./files/texts/textForSlide.txt");
+    try{
+      System.out.println("Type the text for your slide: ");
+      Scanner slideTextTaker = new Scanner(System.in);
+      String slideTest = slideTextTaker.nextLine();
+
+      File textFile = new File("./files/texts/textForSlide.txt");
+      BufferedWriter textFileWriter = new BufferedWriter(new FileWriter(textFile));
+      textFileWriter.write(slideTest);
+      textFileWriter.newLine();
+
+      System.out.println("Ok, so now give me the path to your presentation background image: ");
+      Scanner bgImagePathTaker = new Scanner(System.in);
+      String bgImagePath = bgImagePathTaker.nextLine();
+      String bgImagePathRemakeOne = bgImagePath.replace("'", "");
+      String bgImagePathRemakeTwo = bgImagePathRemakeOne.replace(" ", "");
+      /*BufferedImage bgimg = ImageIO.read(new File(bgImagePath));
+
+      int width = bgimg.getWidth();
+      int height = bgimg.getHeight();*/
+
+      creatingSlide(bgImagePath, textFile);
+    }
+    catch(IOException e) {
+      System.out.println("IO Error");
+    }
+  }
+  public static void creatingSlide(String bgImagePath, File textFile) {
+    JPanel background = new JPanel();
+    JLabel backgroundImage = new JLabel();
+    backgroundImage.setIcon(new ImageIcon(bgImagePath));
+    background.add(backgroundImage);
+    mainWindow.add(background);
+    mainWindow.setVisible(true);
   }
 }
